daafc975260d028371d2768fa1b175f33aca0963
JumaOchi <ochiengjuma95@gmail.com>
2023-01-31 00:20:47 +0000

    pascal triangle pep8 fix

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-
 '''
 function definition for pascal triangle
 '''
+
+
 def pascal_triangle(n):
     '''
     defining pparameters for pascal triangle
     '''
     res = [[1]]
-    if (n <= 0) :
+    if (n <= 0):
         return []
-    else :
-        for i in range(n - 1) :
+    else:
+        for i in range(n - 1):
             temp = [0] + res[-1] + [0]
             row = []
-            for j in range(len(res[-1]) + 1) :
+            for j in range(len(res[-1]) + 1):
                 row.append(temp[j] + temp[j+1])
             res.append(row)
         return res
