1a671f38a60db31099c40e24c3279e85561883bc
JumaOchi <ochiengjuma95@gmail.com>
2023-03-24 01:25:08 +0000

    making channge dyamic programming approach

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Change making module.
+"""
+
+
+def makeChange(coins, total):
+    """Determines the fewest number of coins needed to meet a given
+    amount total when given a pile of coins of different values.
+    """
+    dp = [total + 1] * (total +1)
+    dp[0] = 0
+
+    for a in range(1, total + 1):
+        for c in coins:
+            if a - c >= 0:
+                dp[a] = min(dp[a], 1 + dp[a - c])
+    return dp[total] if dp[total] != total + 1 else -1
